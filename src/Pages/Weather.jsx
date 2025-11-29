deaaa935dc3d417c5d7f2f7732a3e73da1655b94
Hugo Alvarado <haleas03@gmail.com>
2025-11-29 15:31:46 -0600

    feat: skeleton implementation

--- src/Pages/Weather.jsx
+++ src/Pages/Weather.jsx
@@ -16,34 +16,31 @@ const Weather = () => {
       .catch((err) => console.error(err));
   }, []);
 
-
-  if (!weather) {
-    return (
-      
-      <div className="p-6 max-w-sm space-y-4">
-        <h1 className="text-2xl font-semibold mb-2">Clima en Guatemala</h1>
-
-        <Skeleton className="h-5 w-40" />
-        <Skeleton className="h-5 w-32" />
-        <Skeleton className="h-5 w-28" />
-        <Skeleton className="h-5 w-24" />
-        <Skeleton className="h-5 w-48" />
-      </div>
-    );
-  }
-
-
-  const current = weather.current_condition[0];
+  // const current = weather ? weather.current_condition[0] : null;
+  // const current = weather && weather.current_condition[0];
+  const current = weather?.current_condition[0];
 
   return (
     <div className="p-6 max-w-sm bg-white/10 backdrop-blur-md rounded-xl shadow-lg text-white">
       <h1 className="text-2xl font-semibold mb-4">Clima en Guatemala</h1>
 
-      <p><strong>Temperatura:</strong> {current.temp_C}°C</p>
-      <p><strong>Sensación:</strong> {current.FeelsLikeC}°C</p>
-      <p><strong>Humedad:</strong> {current.humidity}%</p>
-      <p><strong>Viento:</strong> {current.windspeedKmph} km/h</p>
-      <p><strong>Descripción:</strong> {current.weatherDesc[0].value}</p>
+      {weather ? (
+        <div>
+          <p><strong>Temperatura:</strong> {current?.temp_C}°C</p>
+          <p><strong>Sensación:</strong> {current?.FeelsLikeC}°C</p>
+          <p><strong>Humedad:</strong> {current?.humidity}%</p>
+          <p><strong>Viento:</strong> {current?.windspeedKmph} km/h</p>
+          <p><strong>Descripción:</strong> {current?.weatherDesc[0].value}</p>
+        </div>
+      ) : (
+        <div>
+          <Skeleton width={32} />
+          <Skeleton width={28} />
+          <Skeleton width={24} />
+          <Skeleton width={28} />
+          <Skeleton width={40} />
+        </div>
+      )}
     </div>
   );
 };
